3570ff2f9ab7dd9f83a6edcbf6d598b1a9aae4d0
admin 1 <admin 1@DESKTOP-5G7O8IS>
2022-02-08 17:50:17 +0530

    Java Program

---
+++ MyProject/src/EqualOperator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EqualOperator {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter a: ");
+		int a = scan.nextInt();
+		System.out.println("enter b: ");
+		int b = scan.nextInt();
+		if(a==b) {
+			System.out.println("both are equal");
+		}
+		if(a!=b) {
+			System.out.println("both are not equal");
+		}
+	}
+}
