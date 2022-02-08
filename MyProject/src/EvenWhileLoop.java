aececf75e3a23060a4f804e2f23dfde3459adda2
admin 1 <admin 1@DESKTOP-5G7O8IS>
2022-02-08 18:18:10 +0530

    Java Program

---
+++ MyProject/src/EvenWhileLoop.java
@@ -0,0 +1,16 @@
+
+public class EvenWhileLoop {
+	public static void main(String[] args) {
+		int a=10;
+		while(a<=100)
+		{
+			if(a%2==0)
+			{
+			System.out.println(a);
+			
+			}
+			a++;
+		}
+			
+	}
+}
